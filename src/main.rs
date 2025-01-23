47849f0ac4efa20cdab384e70bdba0e7909590c0
hyperbora <hyperbora@gmail.com>
2025-01-23 17:25:20 +0900

    feat: bot 시작 코드 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use teloxide::prelude::*;
+use tokio;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl_with_listener(
+        bot.clone(),
+        move |cx: UpdateWithCx<Bot, Message>| async move {
+            cx.requester.send_dice(cx.update.chat.id).send().await?;
+            Ok::<(), Error>(())
+        },
+        teloxide::dispatching::update_listeners::polling_default(bot).await,
+    )
+    .await;
+
+    Ok(())
 }
