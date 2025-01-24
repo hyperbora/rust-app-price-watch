c3d08d1e6c97a758b75c7e496f2313fe021c86eb
hyperbora <hyperbora@gmail.com>
2025-01-24 10:03:22 +0900

    fix: 토큰값 못 가져오는 문제 수정, 봇 테스트 코드 수정

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dotenv;
 use teloxide::prelude::*;
 use tokio;
 
@@ -5,19 +6,16 @@ pub type Error = Box<dyn std::error::Error + Send + Sync>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    dotenv::dotenv().ok();
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
 
     let bot = Bot::from_env();
 
-    teloxide::repl_with_listener(
-        bot.clone(),
-        move |cx: UpdateWithCx<Bot, Message>| async move {
-            cx.requester.send_dice(cx.update.chat.id).send().await?;
-            Ok::<(), Error>(())
-        },
-        teloxide::dispatching::update_listeners::polling_default(bot).await,
-    )
+    teloxide::repl(bot, |u: UpdateWithCx<Bot, Message>| async move {
+        u.requester.send_dice(u.update.chat.id).send().await?;
+        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+    })
     .await;
 
     Ok(())
