cf485a60b799a57d818ce3e7e580097edca1f60b
hyperbora <hyperbora@gmail.com>
2025-07-11 16:47:26 +0900

    패키지 업그레이드, 메인 소스 수정

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use dotenv::dotenv;
-use teloxide::prelude::*;
-use tokio;
-
-type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
 #[tokio::main]
-async fn main() -> HandlerResult {
+async fn main() {
     dotenv().ok();
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting command bot...");
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |cx: UpdateWithCx<Bot, Message>| async move {
-        if let Some(text) = cx.update.text() {
-            cx.answer(format!("You said: {}", text)).send().await?;
+    Command::repl(bot, answer).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Username(username) => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username}.")).await?
         }
-        HandlerResult::Ok(())
-    })
-    .await;
+        Command::UsernameAndAge { username, age } => {
+            bot.send_message(msg.chat.id, format!("Your username is @{username} and age is {age}."))
+                .await?
+        }
+    };
 
     Ok(())
 }
